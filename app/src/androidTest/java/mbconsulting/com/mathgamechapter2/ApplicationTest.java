f694ae2a8ee191290877c34e31143286ae4fcf53
Markus Buschek <Markus Buschek>
2016-09-29 12:01:48 +0200

    Initial commit

---
+++ app/src/androidTest/java/mbconsulting/com/mathgamechapter2/ApplicationTest.java
@@ -0,0 +1,13 @@
+package mbconsulting.com.mathgamechapter2;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
